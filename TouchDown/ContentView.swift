68ee0a2195023a401b79514ec9aaaec547153bc8
PhoonThetPine <thetpine254@gmail.com>
2023-10-15 20:16:13 +0800

    Initial Commit

---
+++ TouchDown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TouchDown
+//
+//  Created by Phoon Thet Pine on 15/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Phoon Thet Pine on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
